40e5d6630ba506276b0ecb62a76bbe22a6d1f3d8
Alen <info@alenbarac.com>
2024-08-27 23:33:06 +0200

    global container component, navbar structure added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
-    <div className="max-w-96 m-auto">
+    <>
       <h2 className="text-3xl mb-4">Homepage</h2>
-      <Button variant="outline" size="lg" className="capitalize">
-        Test button
-      </Button>
-    </div>
+    </>
   )
 }
