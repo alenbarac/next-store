3409b3914750d0122ee2e16afeae0cc8e540b375
Alen <info@alenbarac.com>
2024-09-05 21:42:41 +0200

    next config remote url fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'https://oewpookrhjrbwcwijssx.supabase.co',
+        hostname: 'oewpookrhjrbwcwijssx.supabase.co',
       },
     ],
   },
